cadaa4b02dae1cb52f847a00d0ad8de6cb1879ab
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2026-01-24 07:45:07 -0600

    doc(routes): corrige  documentaicon endpoints routes

--- spots_routes/filters.py
+++ spots_routes/filters.py
@@ -4,7 +4,6 @@ from .models import Route, RoutePhoto
 
 class RouteFilter(django_filters.FilterSet):
     user = django_filters.NumberFilter(field_name='user_id')
-    spot = django_filters.NumberFilter(field_name='spot_id')
     difficulty = django_filters.CharFilter(
         field_name='difficulty__key',
         lookup_expr='iexact'
@@ -17,12 +16,11 @@ class RouteFilter(django_filters.FilterSet):
 
     class Meta:
         model = Route
-        fields = ['user', 'spot', 'difficulty', 'travel_mode']
+        fields = ['user', 'difficulty', 'travel_mode']
         
 class RoutePhotoFilter(django_filters.FilterSet):
-    route = django_filters.NumberFilter(field_name='route_id')
     user = django_filters.NumberFilter(field_name='user_id')
 
     class Meta:
         model = RoutePhoto
-        fields = ['route', 'user']
+        fields = ['user']
