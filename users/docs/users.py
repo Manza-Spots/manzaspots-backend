789ddb0345e6baf00554437c07555d9b3da7fb09
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2026-01-28 14:24:27 -0600

    feat(doc): ordena codigo

--- users/docs/users.py
+++ users/docs/users.py
@@ -3,3 +3,16 @@ from core.docs.core import simple_detail_response
 
 RESPONSE_ACTIVATE_USER = simple_detail_response("Usuario [username] activado.")
 RESPONSE_DESACTIVATE_USER= simple_detail_response("Usuario [username] desactivado.")
+
+RESEND_CONFIRMATION_EMAIL_REQUEST = {
+    "application/json": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "Correo que se verificara"
+            }
+        },
+        "required": ["email"]
+    }
+}
