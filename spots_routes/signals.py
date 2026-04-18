6eb125d55329275c3bd75e4d0e82fe80bda8ad95
morkmorquecho <mork.morquecho@gmail.com>
2026-04-18 07:02:40 -0600

    feature: add new signals

--- spots_routes/signals.py
+++ spots_routes/signals.py
@@ -1,85 +1,82 @@
 # spots_routes/signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+from core.utils.storages import delete_file_fields, delete_if_changed
 from .models import RoutePhoto, Spot, SpotCaption
 import os
 from django.conf import settings
-import os
-from django.db.models.signals import pre_delete, pre_save
-from django.dispatch import receiver
-from django.contrib.gis.db import models
-from django.contrib.gis.db import models as gis_models
-from django.core.validators import FileExtensionValidator
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-@receiver(post_save, sender=Spot)
-def rename_thumbnail_after_create(sender, instance, created, **kwargs):
-    """Renombrar thumbnail después de crear el spot"""
-    if created and instance.spot_thumbnail_path:
-        old_path = instance.spot_thumbnail_path.path
-        
-        # Si el nombre contiene 'temp_', renombrarlo
-        if 'temp_' in os.path.basename(old_path):
-            ext = old_path.split('.')[-1]
-            new_filename = f'spot_{instance.pk}.{ext}'
-            new_path = os.path.join(
-                os.path.dirname(old_path),
-                new_filename
-            )
-            
-            # Renombrar archivo físico
-            if os.path.isfile(old_path):
-                os.rename(old_path, new_path)
-                
-                # Actualizar el campo en la BD
-                instance.spot_thumbnail_path.name = f'spot_thumbnails/{new_filename}'
-                # Usar update() para evitar loop infinito con save()
-                Spot.objects.filter(pk=instance.pk).update(
-                    spot_thumbnail_path=instance.spot_thumbnail_path.name
-                )
+ 
+#=============================== SIGNALS PARA SPOT =======================================
 
-@receiver(pre_delete, sender=Spot)
-def delete_spot_thumbnail(sender, instance, **kwargs):
-    """Eliminar thumbnail cuando se borra un Spot"""
-    if instance.spot_thumbnail_path:
-        try:
-            if os.path.isfile(instance.spot_thumbnail_path.path):
-                os.remove(instance.spot_thumbnail_path.path)
-        except (ValueError, AttributeError, FileNotFoundError):
-            pass
+CAMPOS_SPOT = ['spot_thumbnail_path']
 
 @receiver(pre_save, sender=Spot)
-def delete_old_spot_thumbnail_on_update(sender, instance, **kwargs):
-    """Eliminar thumbnail anterior cuando se actualiza"""
-    if instance.pk:
-        try:
-            old_instance = sender.objects.get(pk=instance.pk)
-            if old_instance.spot_thumbnail_path and old_instance.spot_thumbnail_path != instance.spot_thumbnail_path:
-                if os.path.isfile(old_instance.spot_thumbnail_path.path):
-                    os.remove(old_instance.spot_thumbnail_path.path)
-        except sender.DoesNotExist:
-            pass
-        except (ValueError, AttributeError, FileNotFoundError):
-            pass
+def spot_pre_save(sender, instance, **kwargs):
+    """Maneja eliminación de thumbnail antiguo antes de actualizar"""
+    if not instance.pk:
+        return  # Es un nuevo spot, no hay thumbnail anterior que borrar
+    
+    try:
+        anterior = Spot.objects.get(pk=instance.pk)
+    except Spot.DoesNotExist:
+        return
+    
+    # Borra el thumbnail anterior si cambió
+    delete_if_changed(anterior, instance, CAMPOS_SPOT)
 
-@receiver(pre_delete, sender=SpotCaption)
-def delete_spot_caption_image(sender, instance, **kwargs):
-    """Eliminar imagen cuando se borra un SpotCaption"""
-    if instance.img_path:
-        try:
-            if os.path.isfile(instance.img_path.path):
-                os.remove(instance.img_path.path)
-        except (ValueError, AttributeError, FileNotFoundError):
-            pass
+@receiver(post_delete, sender=Spot)
+def spot_post_delete(sender, instance, **kwargs):
+    """Borra thumbnail cuando se elimina un Spot"""
+    delete_file_fields(instance, CAMPOS_SPOT)
 
-@receiver(pre_delete,sender=RoutePhoto)
-def delete_route_photo_image(sender, instance, **kwargs):
-    """Eliminar imagen cuando se borra un RoutePhoto"""
-    if instance.img_path:
-        try:
-            if os.path.isfile(instance.img_path.path):
-                os.remove(instance.img_path.path)
-        except (ValueError, AttributeError, FileNotFoundError):
-            pass
+
+ 
+#=============================== SIGNALS PARA SPOTCAPTION =======================================
+
+CAMPOS_SPOTCAPTION = ['img_path']
+
+@receiver(pre_save, sender=SpotCaption)
+def spotcaption_pre_save(sender, instance, **kwargs):
+    """Maneja eliminación de imagen antigua antes de actualizar"""
+    if not instance.pk:
+        return
+    
+    try:
+        anterior = SpotCaption.objects.get(pk=instance.pk)
+    except SpotCaption.DoesNotExist:
+        return
+    
+    delete_if_changed(anterior, instance, CAMPOS_SPOTCAPTION)
+
+@receiver(post_delete, sender=SpotCaption)
+def spotcaption_post_delete(sender, instance, **kwargs):
+    """Borra imagen cuando se elimina un SpotCaption"""
+    delete_file_fields(instance, CAMPOS_SPOTCAPTION)
+
+
+#=============================== SIGNALS PARA ROUTEPHOTO =======================================
+
+CAMPOS_ROUTEPHOTO = ['img_path']
+
+@receiver(pre_save, sender=RoutePhoto)
+def routephoto_pre_save(sender, instance, **kwargs):
+    """Maneja eliminación de imagen antigua antes de actualizar"""
+    if not instance.pk:
+        return
+    
+    try:
+        anterior = RoutePhoto.objects.get(pk=instance.pk)
+    except RoutePhoto.DoesNotExist:
+        return
+    
+    delete_if_changed(anterior, instance, CAMPOS_ROUTEPHOTO)
+
+@receiver(post_delete, sender=RoutePhoto)
+def routephoto_post_delete(sender, instance, **kwargs):
+    """Borra imagen cuando se elimina un RoutePhoto"""
+    delete_file_fields(instance, CAMPOS_ROUTEPHOTO)
